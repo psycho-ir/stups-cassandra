451efcb2b06f1a18a858b6b573d6573d9b46a8c9
Ievgen Gusher <ievgen.gusher@zalando.de>
2016-11-09 14:45:37 +0100

    OCTO-1077 re-enable backups

--- cassandra-snapshotter.sh
+++ cassandra-snapshotter.sh
@@ -4,6 +4,8 @@
 DATE=`date +%Y-%m-%d:%H`
 IP=$(curl -Ls -m 4 http://169.254.169.254/latest/meta-data/local-ipv4)
 
+set -x
+
 commando=$1
 keySpaceName=$2
 bucket=$3
@@ -43,25 +45,23 @@ fi
 
 if [ "$commando" == "backup" ]; then
 
-        echo "Describe keyspace $keySpaceName"
-        mkdir -p /opt/recovery/meta/
-        $CASSANDRA_HOME/bin/cqlsh $IP -e "DESC $keySpaceName" > /opt/recovery/meta/$keySpaceName-$DATE.cql
-		aws s3 cp /opt/recovery/meta/$keySpaceName-$DATE.cql s3://$bucket/$APPLICATION_ID-snapshot/$DATE/$IP/$keySpaceName.cql
-       	rm -rfv /opt/recovery/meta/$keySpaceName-$DATE.cql
+	region=`aws s3api get-bucket-location --bucket $bucket | jq -r '.LocationConstraint'`
+  echo "Describe keyspace $keySpaceName"
+  mkdir -p /opt/recovery/meta/
+  $CASSANDRA_HOME/bin/cqlsh $IP -e "DESC $keySpaceName" > /opt/recovery/meta/$keySpaceName-$DATE.cql
+	aws s3 --region $region cp /opt/recovery/meta/$keySpaceName-$DATE.cql s3://$bucket/$APPLICATION_ID-snapshot/$DATE/$IP/$keySpaceName.cql
+ 	rm -rfv /opt/recovery/meta/$keySpaceName-$DATE.cql
+
+  echo "Moving file to S3 Bucket $bucket"
+  for table_dir in `ls -d $backupFolder/* | grep -v feedbacks | grep -v indirectcontextitems` ;
+  do
+      table_name=`echo $table_dir | grep -o "[^\/]*$"`
+      aws s3 --region $region cp $table_dir/snapshots/$snaphshot_dir s3://$bucket/$APPLICATION_ID-snapshot/$DATE/$IP/$keySpaceName/$table_name --recursive
+  done
 
 
-        echo "Moving file to S3 Bucket $bucket"
-        for table_dir in `ls -d $backupFolder/*`;
-        do
-            table_name=`echo $table_dir | grep -o "[^\/]*$"`
-            aws s3 cp $table_dir/snapshots/$snaphshot_dir s3://$bucket/$APPLICATION_ID-snapshot/$DATE/$IP/$keySpaceName/$table_name --recursive
-        done
-
-
-        echo "Cleanup"
- 		rm -rfv $backupFolder/*/snapshots/*
-        echo "Done with snapshot"
+  echo "Done with snapshot"
 else
-		echo "Quit Script"
-		exit 0;
+	echo "Quit Script"
+	exit 0;
 fi
--- snapshot-scheduler.sh
+++ snapshot-scheduler.sh
@@ -2,26 +2,31 @@
 # Script to process backup and restore functionallity in AWS
 # Maintainer: malte.pickhan@zalando.de
 
+set -x 
 while [ -n "$BACKUP_BUCKET" ] ; do
                 #Get pattern from etcd and split into array
                 EXECUTE_PATTERN=($(curl -Ls ${ETCD_URL}/v2/keys/cassandra/${CLUSTER_NAME}/snapshot_pattern | jq -r '.node.value'))
                 MINUTE=$(date +%M)
                 LISTEN_ADDRESS=$(curl -Ls -m 4 http://169.254.169.254/latest/meta-data/local-ipv4)
                 HOUR=$(date +%H)
-                KEYSPACES=($(/opt/cassandra/bin/cqlsh $LISTEN_ADDRESS -e "DESCRIBE KEYSPACES;"))
-                #Remove cassandra default keyspaces
-                KEYSPACES=("${KEYSPACES[@]/system_traces}")
-                KEYSPACES=("${KEYSPACES[@]/system}")
-                        if  [[ "${EXECUTE_PATTERN[0]}" == "?" || "${EXECUTE_PATTERN[0]}" == "$MINUTE" ]] ; then
-                                if [[ "${EXECUTE_PATTERN[1]}" == "?" || "${EXECUTE_PATTERN[1]}" == "$HOUR" ]] ; then
-                                #Execute the actual script for taking a snapshot                              
-                                snapshot_dir=`nodetool snapshot | tail -1| grep -o "[0-9]*"`
-                                for i in "${KEYSPACES[@]}"
-                                        do
-                                        echo "Executing snapshot"
-                                        flock -x -n /var/lock/cassandraSnapshotter.lock /opt/cassandra/bin/cassandra-snapshotter.sh backup $i $BACKUP_BUCKET $snapshot_dir >> /var/log/snapshot_cron.log
-                                        done
-                                fi
-                        fi
-`sleep 1m`
+		KEYSPACES=` /opt/cassandra/bin/cqlsh $LISTEN_ADDRESS -e "DESCRIBE KEYSPACES;" | tr ' ' '\n' | grep -v kairosdb | grep -v system | grep -v '^$'`
+		echo "$KEYSPACES"               
+                for i in `echo $KEYSPACES`
+             	do
+			if [ $HOUR == "05" ]; then
+                #	if  [[ "${EXECUTE_PATTERN[0]}" == "?" || "${EXECUTE_PATTERN[0]}" == "$MINUTE" ]] ; then    
+  	               # 	if [[ "${EXECUTE_PATTERN[1]}" == "?" || "${EXECUTE_PATTERN[1]}" == "$HOUR" ]] ; then
+					TABLES_FULL=`cqlsh $LISTEN_ADDRESS -e "describe keyspace $i" | grep "CREATE TABLE" | awk '{print $3}' | grep -v feedbacks | grep -v indirectcontextitems | tr '\n' ','`
+					TABLE_NAMES=`cqlsh $LISTEN_ADDRESS -e "describe keyspace $i" | grep "CREATE TABLE" | awk '{print $3}' | grep -v feedbacks | grep -v indirectcontextitems |  awk -F'.' '{print $2}' | tr '\n' ' '`
+                        		echo "nodetool flush $i $TABLE_NAMES"
+                        		nodetool flush $i $TABLE_NAMES
+ 					echo "snapshot_dir=`nodetool snapshot -kt $TABLES_FULL | tail -1| grep -o \"[0-9]*\"`"
+ 					snapshot_dir=`nodetool snapshot -kt $TABLES_FULL | tail -1| grep -o "[0-9]*"`
+					echo "Executing snapshot"
+                                        flock -x -n /var/lock/cassandraSnapshotter.lock /opt/cassandra/bin/cassandra-snapshotter.sh backup $i $BACKUP_BUCKET $snapshot_dir
+               				nodetool clearsnapshot
+		#		fi
+			fi
+		done
+	`sleep 45m`
 done
