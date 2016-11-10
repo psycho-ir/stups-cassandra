e349bfe00ca4d433c3d5ebd1af99e13c775fd5f9
Ievgen Gusher <ievgen.gusher@zalando.de>
2016-11-10 13:30:36 +0100

    OCTO-1077 lock before repair

--- repair-scheduler.sh
+++ repair-scheduler.sh
@@ -9,11 +9,23 @@ while true; do
     DAY_EXPR=`expr $DAY + 0`
     # RUN repair every 2nd day once at night
     if  [ $(($DAY_EXPR % 2)) == 0 ] ; then
-        if  [ "03" == "$HOUR" ] ; then
-            /opt/cassandra/bin/nodetool repair -pr
-            # sleep one and a half day
-            `sleep 36h` 
+        if  [ "13" == "$HOUR" ] ; then
+            while true; do
+                curl -Lsf "${ETCD_URL}/v2/keys/cassandra/${CLUSTER_NAME}/repairLock?prevExist=false" \
+                    -XPUT -d value=${LISTEN_ADDRESS} > /dev/null
+                if [ $? -eq 0 ] ;
+                then
+                    /opt/cassandra/bin/nodetool repair -pr
+                    out=$(curl -Lsf ${ETCD_URL}/v2/keys/cassandra/${CLUSTER_NAME}/repairLock?prevValue=${LISTEN_ADDRESS} -XDELETE)
+                    echo "$out"
+                    break
+                else
+                    echo "Failed to acquire repair lock. Waiting for 60 seconds ..."
+                    sleep 10s
+                fi
+            done
+
         fi
     fi
-    `sleep 45m`
+    sleep 45m
 done
