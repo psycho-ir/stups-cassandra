f3fca81c7fce2171101f352995bfb68333066d78
Ievgen Gusher <ievgen.gusher@zalando.de>
2016-11-12 11:04:40 +0100

    OCTO-1077 missing LISTEN_ADDRESS

--- repair-scheduler.sh
+++ repair-scheduler.sh
@@ -3,6 +3,8 @@
 
 set -x
 
+LISTEN_ADDRESS=$(curl -Ls -m 4 http://169.254.169.254/latest/meta-data/local-ipv4)
+
 while true; do
     HOUR=$(date +%H)
     DAY=`date +%d`
@@ -15,7 +17,7 @@ while true; do
                     -XPUT -d value=${LISTEN_ADDRESS} > /dev/null
                 if [ $? -eq 0 ] ;
                 then
-                    /opt/cassandra/bin/nodetool repair -pr
+                    /opt/cassandra/bin/nodetool repair -pr -seq
                     out=$(curl -Lsf ${ETCD_URL}/v2/keys/cassandra/${CLUSTER_NAME}/repairLock?prevValue=${LISTEN_ADDRESS} -XDELETE)
                     echo "$out"
                     break
@@ -24,7 +26,7 @@ while true; do
                     sleep 10s
                 fi
             done
-
+            sleep 36h
         fi
     fi
     sleep 45m
